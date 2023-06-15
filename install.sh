1da59cde623c3f2f6fd907d8203c8915f5dd0e3d
David Cunliffe <52502204+2Wdavidcunliffe@users.noreply.github.com>
2023-06-15 18:37:12 +0000

    updated googlecli

--- install.sh
+++ install.sh
@@ -40,12 +40,12 @@ sudo ./aws/install
 popd
 
 # Install GCloud CLI
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.gpg
-sudo apt update -y
-sudo apt install -y google-cloud-cli
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates gnupg curl sudo
+echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get install -y google-cloud-sdk-gke-gcloud-auth-plugin kubectl google-cloud-sdk-kubectl-oidc
-sudo apt -y --fix-broken install
+# sudo apt -y --fix-broken install
 
 # Install Azure CLI
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/azure-cli.list
